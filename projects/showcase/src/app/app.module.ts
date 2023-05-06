186baec1e5f7cd112595cbe75a07ae353b78524f
dorongruber <doron.gruber@gmail.com>
2023-05-06 18:17:45 +0300

    import library

--- projects/showcase/src/app/app.module.ts
+++ projects/showcase/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DialogModule } from 'dist/ngx-custom-components';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
