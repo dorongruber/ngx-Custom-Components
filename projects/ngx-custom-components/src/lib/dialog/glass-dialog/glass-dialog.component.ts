07c6c7685f7d79b17f57a47623643579829df19b
dorongruber <doron.gruber@gmail.com>
2023-05-06 18:17:03 +0300

    change glass dialog component

--- projects/ngx-custom-components/src/lib/dialog/glass-dialog/glass-dialog.component.ts
+++ projects/ngx-custom-components/src/lib/dialog/glass-dialog/glass-dialog.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges ,Input } from '@angular/core';
 
 @Component({
   selector: 'lib-glass-dialog',
   templateUrl: './glass-dialog.component.html',
   styleUrls: ['./glass-dialog.component.css']
 })
-export class GlassDialogComponent {
+export class GlassDialogComponent implements OnChanges {
 
+  @Input() dialogBackgroundFilter: string = 'blur(5px)'
+  @Input() dialogBorderRadius: string = '15px';
+  @Input() dialogPosition: 'initial' | 'relative' = 'relative';
+  @Input() dialogPadding: string = '2rem';
+  @Input() dialogBorder: string = 'transparent'
+  @Input() dialogWidth: string = 'fit-content';
+
+  private defualtValues = {
+    dialogBorderRadius: '15px',
+    dialogPadding: '2rem',
+    dialogWidth: 'fit-content',
+  };
+  private $scaleRegex = new RegExp("^[0-9]+(rm|rem|px|vh|vw)$");
+
+  ngOnChanges() {
+    this.dialogPadding = this.$scaleRegex.test(this.dialogPadding) ? this.dialogPadding : this.defualtValues.dialogPadding;
+    this.dialogWidth = this.$scaleRegex.test(this.dialogWidth) ? this.dialogWidth : this.defualtValues.dialogWidth;
+  }
 }
